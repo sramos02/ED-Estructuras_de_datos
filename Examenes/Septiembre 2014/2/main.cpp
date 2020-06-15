36e50e47ca13818ac8fb94c1d2440a7457b599e5
Sergio <31887668+sramos02@users.noreply.github.com>
2020-06-15 18:01:37 +0200

    Add files via upload

---
+++ Examenes/Septiembre 2014/2/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include "Arbin.h"
+using namespace std;
+
+//Caso de prueba ([3]25[1])48([3]21#)
+bool arbol_genealogico(const Arbin<int>& arbol) {
+
+    //No tiene hijos //Un solo hijo //Dos hijos
+    if (arbol.hijoIz().esVacio() && arbol.hijoDer().esVacio()) { return true; }
+    else if (arbol.hijoDer().esVacio()) {
+        return arbol_genealogico(arbol.hijoIz()) && (arbol.hijoIz().raiz() + 18 >= arbol.raiz());
+    }
+    else {
+        return arbol_genealogico(arbol.hijoIz()) && arbol_genealogico(arbol.hijoDer()) &&
+            (arbol.hijoIz().raiz() + 18 <= arbol.raiz()) && (arbol.hijoDer().raiz() + 18 <= arbol.raiz()) &&
+            (arbol.hijoIz().raiz() >= arbol.hijoDer().raiz() + 2);
+    }
+}
+
+Arbin<int> lee_arbol(istream& in) {
+    char c;
+    in >> c;
+    switch (c) {
+    case '#': return Arbin<int>();
+    case '[': {
+        int raiz;
+        in >> raiz;
+        in >> c;
+        return Arbin<int>(raiz);
+    }
+    case '(': {
+        Arbin<int> iz = lee_arbol(in);
+        int raiz;
+        in >> raiz;
+        Arbin<int> dr = lee_arbol(in);
+        in >> c;
+        return Arbin<int>(iz, raiz, dr);
+    }
+    default: return Arbin<int>();
+    }
+}
+
+int main() {
+    while (cin.peek() != EOF) {
+        Arbin<int> mapa = lee_arbol(cin);
+        string resto_linea;
+        getline(cin, resto_linea);
+        cout << arbol_genealogico(mapa) << endl;
+    }
+    return 0;
+}
